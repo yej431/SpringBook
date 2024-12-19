37c088f67c5e07ab257ef6047b22f606539041e9
yej431 <42224934+yej431@users.noreply.github.com>
2024-12-19 12:05:15 +0900

    Update PrincipalDetailsService.java

--- SpringBook/src/main/java/com/web/config/auth/PrincipalDetailsService.java
+++ SpringBook/src/main/java/com/web/config/auth/PrincipalDetailsService.java
@@ -15,14 +15,12 @@ public class PrincipalDetailsService implements UserDetailsService {
 	@Autowired
 	private UserRepository userRepository;
 	
-	//스프링이 로그인 요청을 가로챌 때, username, password 변수 2개를 가로채는데
-	//password부분 처리는 알아서 함. username이 DB에 있는지만 확인해주면 됨.
 	@Override
 	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
 		User principal = userRepository.findByUserId(userId)
 				.orElseThrow(()->{
 					return new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다. : "+userId);
 				});
-		return new PrincipalDetail(principal); //시큐리티의 세션에 유저 정보가 저장 됨.
+		return new PrincipalDetail(principal); //시큐리티의 세션에 유저 정보 저장
 	}
 }
