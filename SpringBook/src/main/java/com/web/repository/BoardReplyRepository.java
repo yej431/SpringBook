07a22f1dc0e8f1a8a780899500c293c46dad1fb6
yej431 <42224934+yej431@users.noreply.github.com>
2024-12-19 12:07:58 +0900

    Update BoardReplyRepository.java

--- SpringBook/src/main/java/com/web/repository/BoardReplyRepository.java
+++ SpringBook/src/main/java/com/web/repository/BoardReplyRepository.java
@@ -12,7 +12,7 @@ public interface BoardReplyRepository extends JpaRepository<BoardReply,Integer> 
 
 	@Modifying
 	@Query(value="insert into boardreply(userId, boardId, content, createDate) values(?1,?2,?3,now())", nativeQuery=true)
-	int mSave(int userId, int boardId, String content); //업데이트된 행의 개수를 리턴해준다.
+	int mSave(int userId, int boardId, String content);
 	
 	@Query(value="select id,content,createDate,boardId,userId from boardreply where boardId = ?1"
 			+ " order by id desc limit ?2, ?3", nativeQuery=true)
