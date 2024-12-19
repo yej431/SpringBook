dc2ea8246ce79a73ad689440d4b2134c47d2a9c2
yej431 <42224934+yej431@users.noreply.github.com>
2024-12-19 12:06:04 +0900

    Update SecurityConfig.java

--- SpringBook/src/main/java/com/web/config/SecurityConfig.java
+++ SpringBook/src/main/java/com/web/config/SecurityConfig.java
@@ -13,10 +13,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.web.config.auth.PrincipalDetailsService;
 
-//빈 등록: 스프링 컨테이너에서 객체를 관리할 수 있게 하는 것
-@Configuration //빈등록(IoC관리)
-@EnableWebSecurity //시큐리티 필터로 등록이 된다.
-@EnableGlobalMethodSecurity(prePostEnabled=true) //특정 주소로 접근을 하면 권한 및 인증을 미리 체크하겠다는 뜻.
+@Configuration
+@EnableWebSecurity 
+@EnableGlobalMethodSecurity(prePostEnabled=true) 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
@@ -28,13 +27,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return super.authenticationManagerBean();
 	}
 
-	@Bean //IoC가 된다.
+	@Bean
 	public BCryptPasswordEncoder encodePWD() {
 		return new BCryptPasswordEncoder();
 	}
 	
-	//시큐리티가 대신 로그인해주는데 password를 가로채기를 하는데 해당 password가 뭘로 해쉬가 되어
-	//회원가입 되었는지 알아야 같은 해쉬로 암호화해서 DB에 있는 해쉬랑 비교할 수 있음.
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(principalDetailService).passwordEncoder(encodePWD());
@@ -55,6 +52,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.loginPage("/auth/loginForm")
 				.loginProcessingUrl("/auth/loginProc")
 				.defaultSuccessUrl("/");
-				//스프링 시큐리티가 해당 주소로 요청오는 로그인을 가로채서 대신 로그인 해준다.
 	}
 }
