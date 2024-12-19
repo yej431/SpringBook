9bd31d0cf945eba1e86bf9d8aa8ea9b464bf2805
yej431 <42224934+yej431@users.noreply.github.com>
2024-12-19 12:04:35 +0900

    Update PrincipalDetail.java

--- SpringBook/src/main/java/com/web/config/auth/PrincipalDetail.java
+++ SpringBook/src/main/java/com/web/config/auth/PrincipalDetail.java
@@ -10,8 +10,7 @@ import com.web.model.User;
 
 import lombok.Getter;
 
-//스프링 시큐리티가 로그인 요청을 가로채서 로그인을 진행하고 완료가 되면 UserDetails타입의 오브젝트를
-//스프링 시큐리티의 고유한 세션저장소에 저장을 해준다.
+//스프링 시큐리티의 고유한 세션저장소에 저장.
 @Getter
 public class PrincipalDetail implements UserDetails{
 	private static final long serialVersionUID = 1L;
@@ -31,31 +30,31 @@ public class PrincipalDetail implements UserDetails{
 		return user.getUserId();
 	}
 
-	//계정이 만료되지 않았는지 리턴한다. (true: 만료 안됨)
+	//계정이 만료되지 않았는지 리턴(true: 만료 안됨)
 	@Override
 	public boolean isAccountNonExpired() {
 		return true;
 	}
 
-	//계정이 잠기지 않았는지 리턴한다. (true: 잠기지 않음)
+	//계정이 잠기지 않았는지 리턴(true: 잠기지 않음)
 	@Override
 	public boolean isAccountNonLocked() {
 		return true;
 	}
 
-	//비밀번호가 만료되지 않았는지 리턴한다. (true: 만료 안됨)
+	//비밀번호가 만료되지 않았는지 리턴(true: 만료 안됨)
 	@Override
 	public boolean isCredentialsNonExpired() {
 		return true;
 	}
 
-	//계정이 활성화(사용가능)인지 리턴한다. (true: 활성화)
+	//계정이 활성화(사용가능)인지 리턴(true: 활성화)
 	@Override
 	public boolean isEnabled() {
 		return true;
 	} 
 	
-	//계정이 갖고있는 권한 목록 리턴(권한이 여러개 있을 수 있어서 루프를 돌아야 하는데 여기서는 한개만
+	//계정이 갖고있는 권한 목록 리턴
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		Collection<GrantedAuthority> collectors = new ArrayList<>();
